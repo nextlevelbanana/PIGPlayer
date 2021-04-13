b426950ea6f6de07023853b633ee49c0c377b998
Qristy Overton <qsgiveaways@gmail.com>
2021-04-13 16:02:38 -0700

    clean up manifest, make writing into popup safer

--- popup.js
+++ popup.js
@@ -4,6 +4,6 @@
 *   with the new information.
 */
 browser.runtime.onMessage.addListener(song => {
-  document.getElementById("artist").innerHTML = song.artist;
-  document.getElementById("title").innerHTML = song.title;
+  document.getElementById("artist").textContent = song.artist;
+  document.getElementById("title").textContent = song.title;
 });
